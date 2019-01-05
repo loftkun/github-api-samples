2577dae91bcf605bb9428cbc1dcf12515ae746b1
loftkun <lofkun@gmail.com>
2019-01-06 03:08:59 +0900

    add search

---
+++ search/repositories/search.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+set -eu
+
+# You can use authenticated requests to getting a higher rate limit.
+# See https://developer.github.com/v3/#rate-limiting
+USER=your_username
+PASS=your_password
+
+# GitHub search API Endpoint
+# See https://developer.github.com/v3/search/
+#     https://help.github.com/articles/searching-for-repositories/
+API=https://api.github.com/search/repositories
+
+# Search Params
+# See https://developer.github.com/v3/search/#parameters
+# keywords
+# be careful for uri encode (ex. white space -> + )
+KEYWORD="web+framework"
+#KEYWORD="rest"
+#KEYWORD="router"
+#KEYWORD="micro"
+#KEYWORD="microservice"
+
+# language
+LANGUAGE=go
+
+# lower limit of star num
+STARS=1000
+
+# sort param
+SORT=stars
+ORDER=desc
+
+# item num per page
+# See https://developer.github.com/v3/#pagination
+PER_PAGE=10
+
+# query
+QUERY="${KEYWORD}+in:name,description,readme+language:${LANGUAGE}+stars:>=${STARS}&sort=${SORT}&order=${ORDER}&per_page=${PER_PAGE}"
+
+# curl basic authentication param
+CURL_BASIC="-u ${USER}:${PASS}"
+
+# jq query
+JQ_QUERY=".items[] | [.full_name , .stargazers_count, .html_url, .description] | @csv"
+
+# starting number of pagenation ( 1-origin )
+page=1
+
+while :
+do
+  #echo "page=${page}"
+
+  # search
+  #echo "curl ${CURL_BASIC} \"${API}?q=${QUERY}&page=${page}\""
+  json=$(curl ${CURL_BASIC} "${API}?q=${QUERY}&page=${page}" 2>/dev/null)
+
+  # check result 
+  length=$(echo ${json} | jq -r ".items | length" )
+  if [ $length -eq 0 ]; then
+    break
+  fi
+
+  # echo result
+  echo ${json} | jq -r "${JQ_QUERY}"
+
+  ((page++))
+done
